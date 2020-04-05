c05788dfa68861e81b39840d4af3e798e29cc99b
Gulyuki Aleksey <gulyuki_a@bk.ru>
2020-04-05 13:28:16 +0300

    add Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long amount = 10000;
+        long bonus = amount / 20; {
+        }
+        System.out.println(bonus);
+    }
+}
