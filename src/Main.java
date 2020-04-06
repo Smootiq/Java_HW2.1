d447eb3d0454afa633f10302b5cd074b006e85df
Gulyuki Aleksey <gulyuki_a@bk.ru>
2020-04-06 12:22:43 +0300

    Change Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         long amount = 10000;
-        long bonus = amount / 20; {
-        }
+        long bonus = amount / 20;
         System.out.println(bonus);
     }
 }
